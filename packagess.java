bea3111476f44809321122b2f5e3cf8a4db57326
Sapna981@ <sapnabali45@gmail.com>
2024-01-11 21:06:20 +0530

    dsa problem 1

---
+++ packagess.java
@@ -0,0 +1,15 @@
+/*
+since the class cal in present in package Tools so in order to make use of cal class we need to import it's package as well
+*/
+
+public class packagess{
+    public static void main(String args[]){
+        System.out.println("Packages are basically used for organzing the java files in structural way");
+        /*
+            creating a object for class cal as well as adv_cal
+        */
+       
+       
+    }
+}
+
