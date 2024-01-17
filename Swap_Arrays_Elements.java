6d4d2201b8c34646d5b1c05b8f1e3d70ed92c24a
Sapnabali981 <sapnabali45@gmail.com>
2024-01-18 00:56:17 +0530

    done

--- Swap_Arrays_Elements.java
+++ Swap_Arrays_Elements.java
@@ -8,9 +8,22 @@ public class Swap_Arrays_Elements{
        int a=12;
        int b=13;
        //creating a swap method which can swap the values of a and b for me..
-       swap(a,b);
+       swap(a,b);//here we are creating passing the copies of a and b not passing original variable so that's why once the method is executed swap will happen
+       //after the completion of method the value of a and b is going to remain same..
+       //in order to avoid this problem we will be creating an array and swapping the elements of it based on index value
        System.out.println(a);
        System.out.println(b);
+       int a1[]={2,3,4,5};
+       //here we will be swapping the array elements based on index value now here I want to swap the elements of index 2 with 3
+       int i2=2;
+       int i3=3;
+       SwapArrayElements(a1,i2,i3);
+       System.out.println(a1[2]);
+       System.out.println(a1[3]);
+       /*
+    while swapping the array elements we are making changes in the original copy of array itself here 
+    we are not creating any copies..
+       */
     }
     public static void swap(int a,int b){
         int temp;//using temparory variable I'll be swapping the elements 
@@ -19,5 +32,13 @@ public class Swap_Arrays_Elements{
         b=temp;
         System.out.println(a);
         System.out.println(b);
+    }
+    public static void SwapArrayElements(int a[],int i1,int i2){
+        int temp;
+        temp=a[i1];
+        a[i1]=a[i2];
+        a[i2]=temp;
+        System.out.println(a[i1]);
+        System.out.println(a[i2]);
     }
 }
